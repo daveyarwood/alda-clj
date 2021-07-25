d2514515d698b7ebaf8e4ee4a38fd3ba087327db
Dave Yarwood <dave.yarwood@gmail.com>
2021-07-25 13:41:27 -0400

    Update clj-kondo commit; attempt to remove "redundant do" warnings

--- .clj-kondo/alda/core.clj
+++ .clj-kondo/alda/core.clj
@@ -3,46 +3,50 @@
 
 (defmacro ^:private lisp-builtin
   [sym]
+  ;; `(defn ~sym
+  ;;    "placeholder docstring"
+  ;;    [& ~'args]
+  ;;    "placeholder value")
   `(defn ~sym
-     "placeholder docstring"
+     ~(format "Emits inline Lisp code `(%s ...)`" sym)
      [& ~'args]
-     "placeholder value"))
-  ;;`(defn ~sym
-  ;;   ~(format "Emits inline Lisp code `(%s ...)`" sym)
-  ;;   [& ~'args]
-  ;;   ;; These don't seem to work, but it doesn't really matter that this macro
-  ;;   ;; returns _exactly_ what the real one in alda.core does; the important
-  ;;   ;; thing is just that we `def` something.
-  ;;   ;; (map->Sexp {:form (list* '~sym ~'args)})
-  ;;   ;; (alda.core/map->Sexp {:form (list* '~sym ~'args)})
-  ;;   ;;
-  ;;   ;; ...These don't seem to work either, though:
-  ;;   ;; {:form (list* '~sym ~'args)}))
-  ;;   "placeholder value"))
+     ;; These don't seem to work, but it doesn't really matter that this macro
+     ;; returns _exactly_ what the real one in alda.core does; the important
+     ;; thing is just that we `def` something.
+     (map->Sexp {:form (list* '~sym ~'args)})))
+     ;; (alda.core/map->Sexp {:form (list* '~sym ~'args)})
+     ;;
+     ;; ...These don't seem to work either, though:
+     ;; {:form (list* '~sym ~'args)}))
+     ;; "placeholder value"))
 
 (defmacro ^:private lisp-builtins
   [& syms]
-  (cons 'do
-        (for [sym syms]
-          `(lisp-builtin ~sym))))
+  (if (> (count syms) 1)
+    (cons 'do
+          (for [sym syms]
+            `(lisp-builtin ~sym)))
+    `(lisp-builtin ~(first syms))))
 
 (defmacro ^:private lisp-builtin-attributes
   [& syms]
-  (cons 'do
-        (for [sym syms]
-          `(lisp-builtins ~sym ~(symbol (str sym \!))))))
+  (if (> (count syms) 1)
+    (cons 'do
+          (for [sym syms]
+            `(lisp-builtins ~sym ~(symbol (str sym \!)))))
+    `(lisp-builtins ~(first syms) ~(symbol (str ~(first syms) \!)))))
 
 (defn transform-lisp-builtin
   ""
   [{:keys [node]}]
-  {:node (api/macroexpand lisp-builtin node)})
+  {:node (doto (api/macroexpand lisp-builtin node) prn)})
 
 (defn transform-lisp-builtins
   ""
   [{:keys [node]}]
-  {:node (api/macroexpand lisp-builtins node)})
+  {:node (doto (api/macroexpand lisp-builtins node) prn)})
 
 (defn transform-lisp-builtin-attributes
   ""
   [{:keys [node]}]
-  {:node (api/macroexpand lisp-builtin-attributes node)})
+  {:node (doto (api/macroexpand lisp-builtin-attributes node) prn)})
