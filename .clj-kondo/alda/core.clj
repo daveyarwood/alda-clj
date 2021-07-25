24d460a7266681f4fd07c3e3b08963c8db045574
Dave Yarwood <dave.yarwood@gmail.com>
2021-07-25 15:31:03 -0400

    Clean up commented-out code

--- .clj-kondo/alda/core.clj
+++ .clj-kondo/alda/core.clj
@@ -3,22 +3,10 @@
 
 (defmacro ^:private lisp-builtin
   [sym]
-  ;; `(defn ~sym
-  ;;    "placeholder docstring"
-  ;;    [& ~'args]
-  ;;    "placeholder value")
   `(defn ~sym
      ~(format "Emits inline Lisp code `(%s ...)`" sym)
      [& ~'args]
-     ;; These don't seem to work, but it doesn't really matter that this macro
-     ;; returns _exactly_ what the real one in alda.core does; the important
-     ;; thing is just that we `def` something.
      (map->Sexp {:form (list* '~sym ~'args)})))
-     ;; (alda.core/map->Sexp {:form (list* '~sym ~'args)})
-     ;;
-     ;; ...These don't seem to work either, though:
-     ;; {:form (list* '~sym ~'args)}))
-     ;; "placeholder value"))
 
 (defmacro ^:private lisp-builtins
   [& syms]
